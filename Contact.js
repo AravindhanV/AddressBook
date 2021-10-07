3da6a6dc171eb16c063ecc9227d2506fd7650081
Aravindhan V <varavindhan2010@gmail.com>
2021-10-07 19:41:08 +0530

    ADDED: Regex to validate address, city and state

--- Contact.js
+++ Contact.js
@@ -1,41 +1,58 @@
 class Contact {
-    firstName;
-    lastName;
-    address;
-    city;
-    state;
-    zip;
-    phone;
-    email;
-  
-    constructor(...params) {
-      this.firstName = params[0];
-      this.lastName = params[1];
-      this.address = params[2];
-      this.city = params[3];
-      this.state = params[4];
-      this.zip = params[5];
-      this.phone = params[6];
-      this.email = params[7];
-    }
+  firstName;
+  lastName;
+  address;
+  city;
+  state;
+  zip;
+  phone;
+  email;
+
+  constructor(...params) {
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zip = params[5];
+    this.phone = params[6];
+    this.email = params[7];
   }
-  
-  let firstName = "";
-  let lastName = "";
-  let address = "";
-  let city = "";
-  let state = "";
-  let zip = -1;
-  let phone = -1;
-  let email = "";
-  let contact = new Contact(
-    firstName,
-    lastName,
-    address,
-    city,
-    state,
-    zip,
-    phone,
-    email
-  );
-  
+}
+
+function validateName(name) {
+  return RegExp("^[A-Z][a-z]{2,}$").test(name);
+}
+
+function validateLocation(location) {
+  return RegExp(".{4,}");
+}
+
+let firstName = "";
+if (!validateName(firstName)) throw "Invalid First Name";
+
+let lastName = "";
+if (!validateName(lastName)) throw "Invalid Last Name";
+
+let address = "";
+if (!validateLocation(address)) throw "Invalid address";
+
+let city = "";
+if (!validateLocation(city)) throw "Invalid city";
+
+let state = "";
+if (!validateLocation(state)) throw "Invalid state";
+
+let zip = -1;
+let phone = -1;
+let email = "";
+let contact = new Contact(
+  firstName,
+  lastName,
+  address,
+  city,
+  state,
+  zip,
+  phone,
+  email
+);
