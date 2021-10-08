6b24da0a8f0c3bce47fb136c49dc4c0e84a17c57
Aravindhan V <varavindhan2010@gmail.com>
2021-10-08 16:29:28 +0530

    ADDED: addressBook array to store contacts

--- Contact.js
+++ Contact.js
@@ -1,58 +1,132 @@
 class Contact {
-  firstName;
-  lastName;
-  address;
-  city;
-  state;
-  zip;
-  phone;
-  email;
-
   constructor(...params) {
     this.firstName = params[0];
     this.lastName = params[1];
     this.address = params[2];
     this.city = params[3];
     this.state = params[4];
-    this.zip = params[5];
-    this.phone = params[6];
+    this.zipCode = params[5];
+    this.phoneNumber = params[6];
     this.email = params[7];
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+  set firstName(name) {
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (nameRegex.test(name)) this._firstName = name;
+    else throw "First Name is incorrect";
+  }
+  get lastName() {
+    return this._firstName;
+  }
+  set lastName(name) {
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (nameRegex.test(name)) this._lastName = name;
+    else throw "Last Name is incorrect";
+  }
+  get address() {
+    return this._address;
+  }
+  set address(address) {
+    let addressRegex = RegExp("^[A-Z][a-z]{4,}");
+    if (addressRegex.test(address)) this._address = address;
+    else throw "Address incorrect";
+  }
+  get city() {
+    return this._city;
+  }
+  set city(city) {
+    let cityRegex = RegExp("^[A-Z][a-z]{4,}");
+    if (cityRegex.test(city)) this._city = city;
+    else throw "City incorrect";
+  }
+  get state() {
+    return this._state;
+  }
+  set state(state) {
+    let stateRegex = RegExp("^[A-Z][a-z]{4,}");
+    if (stateRegex.test(state)) this._state = state;
+    else throw "State incorrect";
+  }
+  get zipCode() {
+    return this._zipCode;
+  }
+  set zipCode(zipCode) {
+    let zipCodeRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+    if (zipCodeRegex.test(zipCode)) this._zipCode = zipCode;
+    else throw "ZipCode incorrect";
+  }
+  get phoneNumber() {
+    return this._phoneNumber;
+  }
+  set phoneNumber(phoneNumber) {
+    let phoneRegex = RegExp("[0-9]{2,3}\\s[0-9]{10}$");
+    if (phoneRegex.test(phoneNumber)) this._phoneNumber = phoneNumber;
+    else throw "Phone Number incorrect";
+  }
+  get email() {
+    return this._email;
+  }
+  set email(email) {
+    let emailRegex = RegExp(
+      "[a-zA-Z]{1,}([\\.\\_\\-\\+]?[a-zA-Z0-9]+)@[a-z0-9]+[.][a-z]+[.]?[a-z]{2,3}?$"
+    );
+    if (emailRegex.test(email)) this._email = email;
+    else throw "Email incorrect";
+  }
+
+  toString() {
+    return (
+      "first name=" +
+      this.firstName +
+      ",last name=" +
+      this.lastName +
+      ", address=" +
+      this.address +
+      ", city=" +
+      this.city +
+      ", state=" +
+      this.state +
+      ", zip=" +
+      this.zip +
+      ", phone=" +
+      this.phoneNumber +
+      ", email=" +
+      this.email +
+      "\n"
+    );
   }
 }
 
-function validateName(name) {
-  return RegExp("^[A-Z][a-z]{2,}$").test(name);
+let addressBook = new Array();
+try {
+  addressBook.push(
+    new Contact(
+      "A",
+      "B",
+      "AB Address",
+      "City1",
+      "State1",
+      "560100",
+      "1234567890",
+      "ab@gmail.com"
+    )
+  );
+  addressBook.push(
+    new Contact(
+      "C",
+      "D",
+      "CD Address",
+      "City2",
+      "State1",
+      "560200",
+      "2345678901",
+      "cd@gmail.com"
+    )
+  );
+} catch (e) {
+  console.error(e);
 }
-
-function validateLocation(location) {
-  return RegExp(".{4,}");
-}
-
-let firstName = "";
-if (!validateName(firstName)) throw "Invalid First Name";
-
-let lastName = "";
-if (!validateName(lastName)) throw "Invalid Last Name";
-
-let address = "";
-if (!validateLocation(address)) throw "Invalid address";
-
-let city = "";
-if (!validateLocation(city)) throw "Invalid city";
-
-let state = "";
-if (!validateLocation(state)) throw "Invalid state";
-
-let zip = -1;
-let phone = -1;
-let email = "";
-let contact = new Contact(
-  firstName,
-  lastName,
-  address,
-  city,
-  state,
-  zip,
-  phone,
-  email
-);
+console.log(addressBook);
